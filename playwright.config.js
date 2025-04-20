2ce008beb28c1ecf9d0d69f300e84c8800234ff8
_niezle_ziolko <wgwcompany@duck.com>
2025-04-20 08:28:08 +0200

    Resolved GitHub Actions timeout error

--- playwright.config.js
+++ playwright.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   testDir: './e2e-tests',
   webServer: {
-    command: 'npm run start',
+    command: 'npm run start-prod',
     url: 'http://localhost:5000',
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI
