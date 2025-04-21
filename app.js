11479af6e22f42634ee22552b90353e5c1c03749
_niezle_ziolko <wgwcompany@duck.com>
2025-04-21 09:02:43 +0200

    Add automatic deployemnts and health check endpoint

--- app.js
+++ app.js
@@ -8,4 +8,9 @@ app.use(express.static('dist'))
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
-})
+});
+
+app.get('/health', (req, res) => {
+  if (true) throw('error...');
+  res.send('ok');
+});
